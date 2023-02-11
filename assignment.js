38dde740c5e8cb79cdc2c3d7862aed6f27d6255b
AsiyaAkther <mahicse28@gmail.com>
2023-02-11 19:33:21 +0600

    assignment done

---
+++ assignment.js
@@ -0,0 +1,54 @@
+// ১. Variable কি ?
+    console.log("Q1.Answer: Variables are storage for storing values.")
+
+// ২. Variable কিভাবে লিখতে হয় ?
+    console.log("Q2.Answer: var firstNumber;")
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+    console.log("Q3.Answer: Anything written in a single or double quotes or backticks are string type data. And the variable that carries string type data is a string type variable.")
+    console.log("For ex: var text = 'Hello';")
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+    console.log("Q4.Answer: Numeric values with or without decimals are number type data. And the variable that carries number type data is a number type variable.")
+    console.log("For ex: var number = 45; var number = 45.22;")
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+    console.log("Q5.Answer: Boolean variable has only two posiible values: true or false")
+    console.log("For ex: var data = true;")
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ?
+    console.log("Q6.Answer: toUpperCase() method converted a string to upper case letter.")
+    console.log("var text = 'hello word';")
+    console.log("var result = text.toUpperCase();") // result is = HELLO WORLD
+
+    console.log("Q6.Answer: toLowerCase() method converted a string to lower case letter.")
+    console.log("var text = 'LOWERCASE METHOD';")
+    console.log("var result = text.toLowerCase();") // result is = lowercase method
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+    console.log("Javscript has 5 operators. they are, + - * / %")
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন ।
+    console.log("Q8.Answer: Math.abs() function return a positive value from a negative value.")
+    console.log("var number = -35;")
+    console.log("var result = Math.abs(number);") // the result is 35
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন ।
+    console.log("Q9.Answer: Math.ceil() function returns a decimal number to the next largest integer, It does not metter how many values are given after the decimal point.")
+    console.log("var number = 1.1222;")
+    console.log("var result = Math.ceil(number);") // the result is 2
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন ।
+    console.log("Q10.Answer: Math.floor() function returns a decimal number to the next smallest integer, It does not metter how many values are given after the decimal point.")
+    console.log("var number = 1.99999;")
+    console.log("var result = Math.floor(number);") // the result is 1
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+    console.log("Q11.Answer: Math.round() function returns a decimal number to the integer. If the values are given after decimal point is more than or equal (.5) it returns next largest integer. or, the values are given after decimal point is less then (.5) it returns next smallest integer.")
+    console.log("var number1 = 4.556; var number 2 = 4.335;")
+    console.log("var result = Math.round(number1);") // the result is 5
+    console.log("var result = Math.round(number2);") // the result is 4
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+    console.log("Q12.Answer: Math.random() function returns a random number between 0 and 1.")
+    console.log("var randomNum = Math.random();")
